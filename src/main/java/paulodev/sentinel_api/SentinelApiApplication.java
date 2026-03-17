11fdd973ac5704868809df85680f9b4d70fcf581
Paulo Henrique <paulohenrique1019@gmail.com>
2026-03-17 16:41:20 -0300

    sentinel_api

---
+++ src/main/java/paulodev/sentinel_api/SentinelApiApplication.java
@@ -0,0 +1,13 @@
+package paulodev.sentinel_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentinelApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SentinelApiApplication.class, args);
+    }
+
+}
